82dd18f658bcc8c30fcd5babb5eea414a5d9a973
Eprincev Egor <eprincev.egor@yandex.ru>
2015-05-25 22:59:14 +0300

    fix node

--- index.js
+++ index.js
@@ -2,13 +2,15 @@
 	Возможно здесь когда-нибудь потребуется больше кода...
 */
 
-var classes = requre('./templater.js');
+var classes = require('./lib/templater.js');
 
 // classes = {
 //   StringCoach  -  картека строки
+	exports.StringCoach = classes.StringCoach;
 //   EvalExpression  -  обработка выражений
+	exports.EvalExpression = classes.EvalExpression;
 //   TemplateCoach -> StringCoach  -  расширенная версия каретки
+	exports.TemplateCoach = classes.TemplateCoach;
 //   Templater -> EvalExpression  -  Расширенный обработчик выражений до Шаблонизатора
+	exports.Templater = classes.Templater;
 // }
-
-module.exprots = classes;
